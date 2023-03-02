26ba3b7ce2f836679a7cb65df759b99a6b5b60fd
Jose Casanova <josecasanovaiii@arizona.edu>
2023-03-01 17:52:50 -0700

    Made small changes to PlayOneMP3.java

--- MediaFX/src/PlaySongDemo/PlayAnMP3.java
+++ MediaFX/src/PlaySongDemo/PlayAnMP3.java
@@ -28,13 +28,13 @@ public class PlayAnMP3 extends Application {
   @Override
   public void start(Stage stage) throws Exception {
     BorderPane pane = new BorderPane();
-    String path = "songfiles/Capture.mp3";
-    pane.setCenter( new Label(path));
+    // Jose changed the song before pushing
+    String path = "songfiles/DanseMacabreViolinHook.mp3";    
     playASong(path);
-    // Put the pane in a sized Scene and show the GUI
+    // And changed some lines
     Scene scene = new Scene(pane, 255, 85); // 255 pixels wide, 85 pixels tall
+    pane.setCenter( new Label(path));
     stage.setScene(scene);
-    // Don't forget to show the running app:
     stage.show();
   }
 
